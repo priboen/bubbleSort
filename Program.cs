53e9295d1763b7a71aac7c2b11e735b54952d06a
Muhamad Adri Muwaffaq <muhamad.adri.ft21@mail.umy.ac.id>
2022-10-05 17:28:00 +0700

    method read

--- Program.cs
+++ Program.cs
@@ -6,8 +6,39 @@ using System.Threading.Tasks;
 
 namespace bubbleSort
 {
-    internal class Program
+    class Program
     {
+        private int[] a = new int[20];
+        private int n;
+
+        public void read()
+        {
+            while (true)
+            {
+                Console.WriteLine("Masukan banyaknya elemen pada array : ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 20)
+                    break;
+                else
+                {
+                    Console.WriteLine("\n Array dapat mempunyai maksimal 20 element. \n");
+
+                }
+
+                Console.WriteLine("");
+                Console.WriteLine("-----------------------------------");
+                Console.WriteLine("Masukan elemen pada array : ");
+                Console.WriteLine("-----------------------------------");
+
+                for (int i = 0; i < n; i++)
+                {
+                    Console.Write("<" +  (i + 1) + ">");
+                    string s1 = Console.ReadLine();
+                    a[1] = Int32.Parse(s1);
+                }
+            }
+        }
         static void Main(string[] args)
         {
         }
