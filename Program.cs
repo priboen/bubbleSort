01d58b83fd80e0a66b1c35b48b7a456842047f41
Muhamad Adri Muwaffaq <muhamad.adri.ft21@mail.umy.ac.id>
2022-10-05 17:34:52 +0700

    method BubbleSortArray

--- Program.cs
+++ Program.cs
@@ -48,13 +48,31 @@ namespace bubbleSort
 
             for (int j = 0; j < n; j++)
             {
-                Console.WriteLine($a[j]);
+                Console.WriteLine(a[j]);
             }
             Console.WriteLine("");
 
         }
+
+        public void BubbleSortArray()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j=0; j < n - 1; j++)
+                {
+                    if (a[j] > a[j + 1])
+                    {
+                        int temp;
+                        temp = a[j];
+                        a[j] = a[j + 1];
+                        a[j + 1] = temp;
+                    }
+                }
+            }
+        }
         static void Main(string[] args)
         {
+
         }
     }
 }
